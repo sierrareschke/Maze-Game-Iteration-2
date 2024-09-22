d2431053e4c00f72198aba26148178a7326b0f90
Nolan Brady <nolanrbrady@gmail.com>
2024-09-22 13:11:51 -0600

    Cleaning up the code a bit

--- src/test/java/csci/ooad/MazeTest.java
+++ src/test/java/csci/ooad/MazeTest.java
@@ -9,27 +9,23 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MazeTest {
-    int numberOfRooms = 9;
     List<Room> listOfRooms = new ArrayList<>();
 
     @Test
     public void testNewMazeCreation(){
+        String[] roomNames = {"Room NW", "Room N", "Room NE", "Room W", "Room C", "Room E", "Room SW", "Room S", "Room SE"};
 
-        // Create and assign rooms to the respective positions
-        listOfRooms.add(new Room("Room NW"));  // Empty occupants for now
-        listOfRooms.add(new Room("Room N"));
-        listOfRooms.add(new Room("Room NE"));
-        listOfRooms.add(new Room("Room W"));
-        listOfRooms.add(new Room("Room C"));
-        listOfRooms.add(new Room("Room E"));
-        listOfRooms.add(new Room("Room SW"));
-        listOfRooms.add(new Room("Room S"));
-        listOfRooms.add(new Room("Room SE"));
+        int roomIndex = 0;
+        for (String name: roomNames){
+            listOfRooms.add(new Room(name,roomIndex));
+            roomIndex++;
+        }
 
-        Maze maze = new Maze(numberOfRooms, listOfRooms);
+        Maze maze = new Maze(listOfRooms.size(), listOfRooms);
 
         assertNotEquals(0, maze.numberOfRooms);
         assertNotEquals(false, maze.isSquareMatrix);
+        assertEquals(3, maze.grid.length, "Matrix should have 3 rows");
         System.out.println(Arrays.deepToString(maze.grid));
         // Need to assert that the maze is the correct dimesions
     }
