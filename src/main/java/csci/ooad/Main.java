8536569fe2b00a070ab2a99864d187127796aae4
ohlsengrace <ohlsengrace@gmail.com>
2024-09-24 17:09:45 -0600

    Logger and LoggerFactory successfully recognized

--- src/main/java/csci/ooad/Main.java
+++ src/main/java/csci/ooad/Main.java
@@ -1,12 +1,6 @@
 package csci.ooad;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
 public class Main {
-
-    private static final Logger logger = LoggerFactory.getLogger(Adventurer.class);
 
 
     public static void main(String[] args) {
